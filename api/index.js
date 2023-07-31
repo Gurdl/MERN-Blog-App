8eb62340eff7d0221524a461514e5bc6d2a30686
Gurdev Singh <97690086+Gurdl@users.noreply.github.com>
2023-07-31 13:50:14 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const app=express();
+const fs = require("fs");
 const dotenv=require("dotenv")
 var cors = require('cors')
 const mongoose=require("mongoose")
@@ -32,6 +33,43 @@ const upload=multer({storage:storage});
 app.post("/api/upload",upload.single("file"),(req,res)=>{
    res.status(200).json("File has been uploaded")
 })
+//To see all the images:
+app.get("/api/images/list", (req, res) => {
+  const imagesDirectory = path.join(__dirname, "images");
+
+  // Read the contents of the "images" directory
+  fs.readdir(imagesDirectory, (err, files) => {
+    if (err) {
+      console.error("Error reading images directory:", err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+
+    // Filter out non-image files if needed (optional)
+    // Example: Only return files with ".jpg" or ".png" extensions
+    const imageFiles = files.filter((file) => {
+      return /\.(jpg|jpeg|png|gif)$/i.test(file);
+    });
+
+    res.status(200).json(imageFiles);
+  });
+});
+//Get image by name
+app.get("/api/images/:filename", (req, res) => {
+  const filename = req.params.filename;
+  const imagePath = path.join(__dirname, "images", filename);
+
+  // Check if the file exists
+  if (!fs.existsSync(imagePath)) {
+    return res.status(404).json({ error: "Image not found" });
+  }
+
+  // Set the content type header to "image/jpeg" (you can change this based on your image type)
+  res.set("Content-Type", "image/jpeg");
+
+  // Send the image as the response
+  res.sendFile(imagePath);
+});
+
 
 
 app.use("/api/auth",authRoute)
