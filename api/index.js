c880597c4cf384388068fe7085b1c5f196d96f5b
Gurdev <Gurdevsingh95801@gmail.com>
2023-08-01 09:30:49 -0700

    Fixed the method get Images ByName

--- api/index.js
+++ api/index.js
@@ -56,37 +56,7 @@ app.get("/api/images/list", (req, res) => {
     res.status(200).json(imageFiles);
   });
 });
-//Get image by name
-app.get("/api/images/:filename", (req, res) => {
-  const filename = req.params.filename;
-  const imagePath = path.join(__dirname, "images", filename);
 
-  // Check if the file exists
-  if (!fs.existsSync(imagePath)) {
-    return res.status(404).json({ error: "Image not found" });
-  }
-
-  // Set the content type header to "image/jpeg" (you can change this based on your image type)
-  res.set("Content-Type", "image/jpeg");
-
-  // Send the image as the response
-  res.sendFile(imagePath);
-  // Read the contents of the "images" directory
-  fs.readdir(imagesDirectory, (err, files) => {
-    if (err) {
-      console.error("Error reading images directory:", err);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-
-    // Filter out non-image files if needed (optional)
-    // Example: Only return files with ".jpg" or ".png" extensions
-    const imageFiles = files.filter((file) => {
-      return /\.(jpg|jpeg|png|gif)$/i.test(file);
-    });
-
-    res.status(200).json(imageFiles);
-  });
-});
 //Get image by name
 app.get("/api/images/:filename", (req, res) => {
   const filename = req.params.filename;
